529e8c5f23f4f6fb727f501e350a44f399c54962
VadimCine <aka-sem@yandex.ru>
2025-01-20 20:10:29 +0300

    Add files via upload

---
+++ module_13_4.py
@@ -0,0 +1,47 @@
+from cgitb import handler
+
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.dispatcher import FSMContext
+import asyncio
+
+from module_13_2 import start
+
+api = "7701027879:AAHCWlz67A3rRVjM5ts7ybe5DfPDhhyfIzw"
+bot = Bot(token=api)
+db = Dispatcher(bot, storage = MemoryStorage())
+
+class UserState(StatesGroup):
+    age = State()
+    growth = State()
+    weight = State()
+
+@db.message_handler(text = "Calories")
+async def set_age(message):
+    await message.answer("Введите свой возраст:")
+    await UserState.age.set()
+
+@db.message_handler(state = UserState.age)
+async def set_growth(message, state):
+    await state.update_data(age = message.text)
+    await message.answer("Введите свой рост:")
+    await UserState.growth.set()
+
+@db.message_handler(state = UserState.growth)
+async def set_weight(message, state):
+    await state.update_data(growth=message.text)
+    await message.answer("Введите свой вес:")
+    await UserState.weight.set()
+
+@db.message_handler(state = UserState.weight)
+async def send_calories(message, state):
+    await state.update_data(weight=message.text)
+    data = await state.get_data()
+    result = float(10 * int(data['weight']) + 6.25 * int(data['growth']) - 5 * int(data['age']) + 5)
+    await message.answer(f'Ваша норма калорий - {result}')
+    await state.finish()
+
+if __name__ == "__main__":
+    executor.start_polling(db, skip_updates=True)
+
